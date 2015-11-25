6fd8c858dbb6c55e965ce6dfc433c87b48d69725
Ian Blumenfeld <iblumenfeld@cyberpointllc.com>
2015-11-25 14:32:31 -0500

    Added stub for Merkle c code.

---
+++ merkle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//Current placeholder for merkly code in c
+int lamport_key_gen(){
+  return 0;
+}
+
+int lamport_sign(){
+  return 0;
+}
+
+int lamport_verify(){
+  return 0;
+}
+
+int main(){
+  return 0;
+}
